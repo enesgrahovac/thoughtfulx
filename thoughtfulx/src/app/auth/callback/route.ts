d5048f6f3615f597e5c081fb63a496bd701109be
Enes Grahovac <enesxgrahovac@gmail.com>
2024-07-26 16:49:44 -0400

    refactoring login logic

--- thoughtfulx/src/app/auth/callback/route.ts
+++ thoughtfulx/src/app/auth/callback/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from 'next/server'
 import { createClient } from '@/utils/supabase/auth/server'
 
 export async function GET(request: Request) {
+    console.log("request",request)
     console.log("in auth callback")
     const { searchParams, origin } = new URL(request.url)
     console.log("searchParams", searchParams)
